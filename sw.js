9b9d67694094081ed19f04cc3fe6ac8c442d492f
umedturshiev-creator <umedturshiev@gmail.com>
2026-04-11 15:45:00 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME="uza-app";
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE_NAME).then(c=>c.addAll(["./","./index.html"]))
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
+);
+});
